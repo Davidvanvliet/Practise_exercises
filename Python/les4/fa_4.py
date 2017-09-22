b27c8b83a1fd89d5a3717ca423b5cace89abff19
David <david.vanvliet@student.hu.nl>
2017-09-22 12:57:00 +0200

    .

--- Python/les4/fa_4.py
+++ Python/les4/fa_4.py
@@ -2,28 +2,31 @@
 #David van Vliet
 
 def standaardprijs(afstandKM) :
-    0.80 * afstandKM
+    prijs = 0.80 * afstandKM
     if afstandKM > 50 :
-        0.60 * afstandKM + 15
+        prijs = 0.60 * afstandKM + 15
     if afstandKM < 0 :
-        0.00 * afstandKM
+        prijs = 0.00 * afstandKM
+    return prijs
 
 def ritprijs(leeftijd, weekendrit, afstandKM) :
-    ritprijs == standaardprijs
-    if leeftijd < 12 :
-        ritprijs/100*70
-    else :
-        ritprijs == standaardprijs
-    if leeftijd >= 65 :
-        ritprijs/100*70
-    else :
-        ritprijs == standaardprijs
-    if weekendrit is True :
+    ritprijs = standaardprijs(afstandKM)
+    if weekendrit == 'ja' or weekendrit == 'Ja' :
         if leeftijd < 12 or leeftijd >= 65 :
-            ritprijs/100*70
+           prijs =  (ritprijs/100*65)
         else :
-            ritprijs/100*70
+             prijs = (ritprijs/100*60)
     else :
-        ritprijs == standaardprijs
-    return ritprijs
-print(ritprijs(20, False, 25))
+        if leeftijd < 12 or leeftijd >= 65 :
+             prijs = (ritprijs/100*70)
+        else :
+            prijs = standaardprijs(afstandKM)
+    return print(prijs)
+
+leeftijd = 11
+weekendrit = 'ja'
+afstandKM = 60
+
+ritprijs(leeftijd, weekendrit, afstandKM)
+
+print (ritprijs)
