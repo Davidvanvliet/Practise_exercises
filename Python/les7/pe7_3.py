1375cac36e550cc231a964c16d3f906c69f38c87
David <david.vanvliet@student.hu.nl>
2017-10-02 11:09:29 +0200

    .

--- Python/les7/pe7_3.py
+++ Python/les7/pe7_3.py
@@ -2,5 +2,5 @@
 #David van Vliet
 
 klas = {'piet': 6, 'henk': 7, 'sjors': 8, 'klaas': 9, 'kees': 10, 'gert': 4, 'gerard': 10, 'anton': 7}
-klas = dict((x, y) for x, y in klas.items() if y >= 9)
+klas = dict((k, v) for k, v in klas.items() if v >= 9)
 print(klas)
