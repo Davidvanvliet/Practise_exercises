436d7ed7454e76f32aa800ca193455cdd0328614
David <david.vanvliet@student.hu.nl>
2017-10-05 13:38:53 +0200

    .

--- Python/les10/test.py
+++ Python/les10/test.py
@@ -1,1 +1,15 @@
 import xmltodict
+
+def processXML(filename):
+    with open(filename) as myXMLFile:
+        filestring = myXMLFile.read()
+        xmldictionary = xmltodict.parse(filestring)
+        return xmldictionary
+
+boeken = processXML('boeken.xml')
+bookdict = boeken ['catalog']['book']
+for book in bookdict:
+    author = book['author']
+    title = book['title']
+    price = book['price']
+    print(author, title, price)
