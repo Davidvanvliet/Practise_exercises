700da5e702306845b1107535cd88d9ef7f6d69c1
David <david.vanvliet@student.hu.nl>
2017-10-05 11:44:32 +0200

    .

--- Python/les8/fa8.py
+++ Python/les8/fa8.py
@@ -3,7 +3,7 @@
 
 stations = ['schagen', 'heerhugowaard', 'alkmaar', 'castricum', 'zaandam', 'amsterdam sloterdijk', 'amsterdam centraal', 'amsterdam amstel', 'utrecht centraal', '’s-hertogenbosch', 'eindhoven', 'weert', 'roermond', 'sittard', 'maastricht']
 
-def inlezen_beginstation(stations):
+def inlezen_beginstation(stations):     #input krijgen voor het beginstation. Als het een foute invoer heeft, opnieuw vragen
     beginstation = input("Wat is je begin station?")
     while beginstation not in stations:
         print("wrong station")
@@ -12,12 +12,12 @@ def inlezen_beginstation(stations):
 
 beginstation = inlezen_beginstation(stations)
 
-def inlezen_eindstation(stations, beginstation):
+def inlezen_eindstation(stations, beginstation):    #input krijgen voor het eindstation. Als het een foute invoer heeft, opnieuw vragen.
     eindstation = input("Wat is je eind station?")
     while eindstation not in stations:
        print("wrong station")
        eindstation = input("Wat is je eind station?")
-    while stations.index(eindstation) < stations.index(beginstation):
+    while stations.index(eindstation) < stations.index(beginstation):       #Als het eindstation voor het beginstation staat opnieuw vragen
         print("Uw eindstation staat voor het beginstation. Dit kan niet!")
         eindstation = input("Wat is je eind station?")
     return eindstation
@@ -25,17 +25,16 @@ def inlezen_eindstation(stations, beginstation):
 eindstation = inlezen_eindstation(stations, beginstation)
 
 def omroepen_reis(stations, beginstation, eindstation):
-    positie1 = stations.index(beginstation) +1
-    positie2 = stations.index(eindstation) +1
-    positie3 = stations.index(beginstation) +1
+    positie1 = stations.index(beginstation) +1      #Beginstation index
+    positie2 = stations.index(eindstation) +1       #Eindstation index
     print("Het beginstation ", beginstation, "is het ", positie1, "e station in het traject")
     print("Het eindstation ", eindstation, "is het ", positie2, "e station in het traject")
-    afstand = positie2 - positie1
+    afstand = positie2 - positie1       #afstand berekenen door het eindstation index - beginstation index te berekenen
     print("De afstand bedraagt ", afstand, "station(s)")
-    prijs = afstand * 5
+    prijs = afstand * 5     #Prijs van het ticket berekenen door de afstand (index) * 5 uit te rekenen
     print("De prijs van het kaartje is ", prijs, "euro")
     print("Jij stapt in de trein in ", beginstation)
-    print(*stations[positie3:positie2 -1], sep='\n - ')
+    print(*stations[positie1:positie2 -1], sep='\n - ')     #tussenstations printen
     print("Jij stapt uit in ", eindstation)
 
 print(omroepen_reis(stations, beginstation, eindstation))
